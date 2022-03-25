cb645114f875ea0d4362d72a21b3f900cabc8808
huangxiangquan <huangxiangquan@163.com>
2022-03-25 14:52:47 +0800

    Netty重连和心跳检测

---
+++ dps-netty/src/main/java/cn/xqhuang/dps/heartbeat/HeartBeatClientHandler.java
@@ -0,0 +1,16 @@
+package cn.xqhuang.dps.heartbeat;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class HeartBeatClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.out.println(" client received :" + msg);
+        if (msg != null && msg.equals("idle close")) {
+            System.out.println(" 服务端关闭连接，客户端也关闭");
+            ctx.channel().closeFuture();
+        }
+    }
+}
