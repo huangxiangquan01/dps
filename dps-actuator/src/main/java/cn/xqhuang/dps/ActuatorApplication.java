7668a316b9c4fa60f4ebd98e70bdc2e0783c673b
huangxiangquan <huangxiangquan@163.com>
2022-08-02 10:17:15 +0800

    springBootAdmin监控集成

---
+++ dps-actuator/src/main/java/cn/xqhuang/dps/ActuatorApplication.java
@@ -0,0 +1,12 @@
+package cn.xqhuang.dps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class);
+    }
+}
