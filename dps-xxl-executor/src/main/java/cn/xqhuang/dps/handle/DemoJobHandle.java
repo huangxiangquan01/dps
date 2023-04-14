b832cb8aad4bb407b58823496f903bef680536dc
huangxiangquan <huangxiangquan@163.com>
2023-04-14 10:45:13 +0800

    fix: 新增xxl-job 2.4.1 集成示例

---
+++ dps-xxl-executor/src/main/java/cn/xqhuang/dps/handle/DemoJobHandle.java
@@ -0,0 +1,23 @@
+package cn.xqhuang.dps.handle;
+
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author huangxq
+ * @description:
+ * @date 2023/4/14 10:16 星期五
+ */
+@Component
+public class DemoJobHandle {
+
+    private static Logger logger = LoggerFactory.getLogger(DemoJobHandle.class);
+
+    @XxlJob("demoJobHandler")
+    public void execute() throws Exception {
+        logger.info("XXL_JOB, hello world");
+    }
+}
