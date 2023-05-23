59030a378b27db780fabbf79f66a87bae2aa70ab
huangxiangquan <huangxiangquan@163.com>
2023-05-23 21:27:58 +0800

    fix: 状态机

---
+++ dps-framework/dps-statemachine/src/main/java/cn/xqhuang/dps/StatemachineApplication.java
@@ -0,0 +1,15 @@
+package cn.xqhuang.dps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangxq
+ * @date 2023/4/26 09:13
+ */
+@SpringBootApplication
+public class StatemachineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatemachineApplication.class);
+    }
+}
