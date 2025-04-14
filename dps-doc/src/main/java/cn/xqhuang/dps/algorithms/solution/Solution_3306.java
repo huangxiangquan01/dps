087b2b03f4e993a5591de8fbdbd56f46364b846d
huangxiangquan <huangxiangquan@163.com>
2025-04-14 10:58:05 +0800

    fix: Leetcode 3306

---
+++ dps-doc/src/main/java/cn/xqhuang/dps/algorithms/solution/Solution_3306.java
@@ -0,0 +1,18 @@
+package cn.xqhuang.dps.algorithms.solution;
+
+/**
+ 给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。
+
+ 一次操作中，你将执行：
+
+ 选择 nums 中最小的两个整数 x 和 y 。
+ 将 x 和 y 从 nums 中删除。
+ 将 min(x, y) * 2 + max(x, y) 添加到数组中的任意位置。
+ 注意，只有当 nums 至少包含两个元素时，你才可以执行以上操作。
+
+ 你需要使数组中的所有元素都大于或等于 k ，请你返回需要的 最少 操作次数。
+
+
+ */
+public class Solution_3306 {
+}
